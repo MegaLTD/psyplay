7088722672d2716258a0ff25b8d733951310b79e
MegaLTD <102382969+MegaLTD@users.noreply.github.com>
2023-01-19 18:23:10 +0100

    Add files via upload

--- MegaWatch/Al-Kabeer-Awi-7/E2/4nJVQrVpvU.js
+++ MegaWatch/Al-Kabeer-Awi-7/E2/4nJVQrVpvU.js
@@ -2,16 +2,16 @@ function RI(min, max){
 		return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 function server1(){
-	document.getElementById("Ep2").setAttribute("src", " ");
+	document.getElementById("Ep2").setAttribute("src", "https://www.youtube.com/embed/Axs3ta4KD48");
 }
 function server2(){
-	document.getElementById("Ep2").setAttribute("src", " ");
+	document.getElementById("Ep2").setAttribute("src", "https://www.youtube.com/embed/Axs3ta4KD48");
 }
 function server3(){
-	document.getElementById("Ep2").setAttribute("src", " ");
+	document.getElementById("Ep2").setAttribute("src", "https://www.youtube.com/embed/Axs3ta4KD48");
 }
 function server4(){
-	document.getElementById("Ep2").setAttribute("src", " ");
+	document.getElementById("Ep2").setAttribute("src", "https://www.youtube.com/embed/Axs3ta4KD48");
 }
 function randB(){
 	let i = RI(0, 9);
